c16f46a652959eeb4ac46df90e7283de111ddad2
Harry Northam <hello@harrynortham.com>
2024-08-07 09:29:58 +0200

    added dev source maps and dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,32 @@
 const path = require("path");
+const toml = require("toml");
+const yaml = require("yamljs");
+const json5 = require("json5");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
+  entry: {
+    index: "./src/index.js",
+    print: "./src/print.js",
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Output Management",
+    }),
+  ],
   output: {
-    filename: "bundle.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
+    publicPath: "/",
+  },
+  optimization: {
+    runtimeChunk: "single",
   },
   module: {
     rules: [
@@ -20,6 +41,35 @@ module.exports = {
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: "asset/resource",
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ["csv-loader"],
+      },
+      {
+        test: /\.xml$/i,
+        use: ["xml-loader"],
+      },
+      {
+        test: /\.toml$/i,
+        type: "json",
+        parser: {
+          parse: toml.parse,
+        },
+      },
+      {
+        test: /\.yaml$/i,
+        type: "json",
+        parser: {
+          parse: yaml.parse,
+        },
+      },
+      {
+        test: /\.json5$/i,
+        type: "json",
+        parser: {
+          parse: json5.parse,
+        },
       },
     ],
   },
